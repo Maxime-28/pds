ca747d1cca26cd309512ab5152a2ac8495fda094
Carlos Requena López <crequena@ulb.ac.be>
2016-11-28 17:43:01 +0100

    testing authentication ulb from heroku 1

--- app/authenticators/ulb.js
+++ app/authenticators/ulb.js
@@ -2,13 +2,47 @@ import Ember from 'ember';
 import Base from 'ember-simple-auth/authenticators/base';
 
 export default Base.extend({
-  /* store: Ember.inject.service();
-   * restore(data) {
-   * },
+  store: Ember.inject.service('store'),
+  tokenEndpoint: 'http://www.ulb.ac.be/commons/check',
 
-   * authenticate() {
-  * },
+  restore(data) {
+    return new Ember.RSVP.Promise(function(resolve, reject) {
+      if (!Ember.isEmpty(data.token)) {
+        resolve(data);
+      } else {
+        reject();
+      }
+    });
+  },
 
-* invalidate(data) {
-  * }*/
+  authenticate(options) {
+    return new Ember.RSVP.Promise((resolve, reject) => {
+      Ember.$.ajax({
+        url: this.tokenEndpoint,
+        type: 'GET',
+	data: {
+          _sid: "asdf",
+          _uid: "qert",
+	  _type: "normal"
+        },
+        contentType: 'text/xml',
+        dataType: 'json'
+      }).then(function(response) {
+        Ember.run(function() {
+          resolve({
+            token: response.id_token
+          });
+        });
+      }, function(xhr, status, error) {
+        var response = xhr.responseText;
+        Ember.run(function() {
+          reject(response);
+        });
+      });
+    });
+  },
+
+  invalidate(data) {
+
+  }
 });
--- app/controllers/login.js
+++ app/controllers/login.js
@@ -1,11 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  session: Ember.inject.service('session'),
   queryParams: ['_sid', '_uid'],
 
   actions: {
     authenticate() {
-      console.log("adsf");
+
+      var sid = this.get('_sid');
+      var uid = this.get('_uid');
+
+      console.log(uid);
+      this.get('session').authenticate('authenticator:ulb', sid, uid).catch((reason) => {
+        this.set('errorMessage', reason.error || reason);
+      });
     }
   }
 });
